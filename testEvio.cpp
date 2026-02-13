900f59101e9fa62e721967ae271e952c6f271b53
LassiW96 <lasithagayan9596@gmail.com>
2026-02-13 13:07:23 -0600

    testEvio script modified, stopped tracking some unneccessary fileds.

--- testEvio.cpp
+++ testEvio.cpp
@@ -3,18 +3,15 @@
 #include <TH1F.h>
 #include <TCanvas.h>
 #include <TVirtualPad.h>
-// #include <iostream>
-// #include <vector>
 
-R__LOAD_LIBRARY(/home/lasitha/MyFiles/Root/proj2-SDK/install/lib/libV1720.so)
+R__LOAD_LIBRARY({CMAKE_INSTALL_PREFIX}/lib/libV1720.so)
 
-//#include "V1720FileSetup.h"
 #include "V1720EvioDecode.h"
 
 using namespace std;
 
 void testEvio() {
-    const string evioFile = "ADCTest_1102.evio.0";
+    const string evioFile = "example.evio.0";
     V1720EvioDecode decoder(evioFile);
     decoder.decode();
 }
